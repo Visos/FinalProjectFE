5f8714a2565f3b6f6c04696ae7d2da0f5e2e6555
Visos <visinoni.giacomo99@gmail.com>
2024-08-07 15:54:21 +0200

    progetto vuoto

---
+++ src/test/java/com/example/fe/FinalProjectFE/FinalProjectFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fe.FinalProjectFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
